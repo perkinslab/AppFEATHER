5651c2249975eeb1bdc7d43091c64cf2fbe097a7
Your Name <patirck.heenan@colorado.edu>
2018-02-10 15:14:36 -0700

    minor plot change

--- Code/_no_event.py
+++ Code/_no_event.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals
 # This file is used for importing the common utilities classes.
 import numpy as np
 import matplotlib.pyplot as plt
-from Code import Analysis
+from . import Analysis
 from itertools import chain
 
 class prediction_info:
